7e8a569f294761f0a781b4b4eab2486269ee2f2b
gpradinett <gpradinett@gmail.com>
2022-02-03 21:10:07 -0800

    a C program that prints exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
 /**
  * main -  print Programming is like building a multilingual puzzle
  *
  * Description: "Programming is like building a multilingual puzzle"
  *
- * return : 0 regresa
+ * Return: Always 0 (two success)
  */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
