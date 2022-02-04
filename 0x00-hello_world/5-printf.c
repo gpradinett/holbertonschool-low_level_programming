b40194e8e5d86dfc397e954b3c9ebc573efaa931
gpradinett <gpradinett@gmail.com>
2022-02-03 20:31:56 -0800

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  print Programming is like building a multilingual puzzle
+ * return : 0
+ **/
+int  main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
