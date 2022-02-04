be29dc90cbc2080aa0ff60787f24c57ad66a7ec4
gpradinett <gpradinett@gmail.com>
2022-02-04 15:01:06 -0800

    negative and positive random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	printf("%d is pocleasitive", n);
+	printf("%d is pocleasitive"\n, n);
 	else if (n == 0)
-	printf("%d is zero ", n);
+	printf("%d is zero "\n, n);
 	else if (n < 0)
-	printf("%d is negative", n);
+	printf("%d is negative"\n, n);
 	return (0);
 }
