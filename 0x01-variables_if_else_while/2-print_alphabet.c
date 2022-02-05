8cab68d006d8e221310c8158f4b1d764861dffc4
gpradinett <gpradinett@gmail.com>
2022-02-04 19:58:20 -0800

    program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - random number negative and positive random number
+ *
+ * Return: Always 0 (two success)
+ *
+ * Description: negative and positive random number
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return(0);
+}
