ef85e946094fae61d995c4ff9eea66cc922eb011
gpradinett <gpradinett@gmail.com>
2022-02-04 21:01:08 -0800

    program that prints all single digit numbers of base 10 starting from 0, followed by a new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - random number negative and positive random number
+ *
+ * Return: Always 0 (two success)
+ *
+ * Description: negative and positive random number
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
