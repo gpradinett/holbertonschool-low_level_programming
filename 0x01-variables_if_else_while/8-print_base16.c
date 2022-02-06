c221dd3f06a71d1d14254caf5833976136701d00
gpradinett <gpradinett@gmail.com>
2022-02-05 18:51:46 -0800

    program that prints all the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - random number negative and positive random number
+ *
+ * Return: Always 0 (two success)
+ *
+ * Description: negative and positive random number
+ */
+int main(void)
+{
+yint n;
+char ch;
+
+for (n = 48; n < 58; n++)
+{
+putchar (n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar (ch);
+}
+putchar('\n');
+return (0);
+}
