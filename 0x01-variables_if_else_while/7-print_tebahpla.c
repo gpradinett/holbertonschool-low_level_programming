bb9aa1ca5de72771564088035ceb23e54a2ad1ed
gpradinett <gpradinett@gmail.com>
2022-02-05 18:43:45 -0800

    program that prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - random number negative and positive random number
+ *
+ * Return: Always 0 (two success)
+ *
+ * Description: negative and positive random number
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+
+	{
+	putchar(ch);
+}
+	putchar('\n');
+	return (0);
+}
