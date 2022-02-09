ca337b47a047013bf44358dd296e3f1d30b4fc6a
gpradinett <94254671+gpradinett@users.noreply.github.com>
2022-02-08 23:46:17 -0500

    a function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include <main.h>
+/**
+ * main - random number negative and positive random number
+ *
+ * Return: Always 0 (two success)
+ *
+ * Description: negative and positive random number
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+  int i;
+  
+  i = 0;
+  
+	while (i < 10)
+{
+    ch = 'a';
+    while (ch <= 'z')
+    {
+    _putchar(ch);
+		ch++;
+    }
+}
+	_putchar('\n');
+	return (0);
+}
