db53b367428941586c05e85a6c060c6ee5d6bba7
gpradinett <94254671+gpradinett@users.noreply.github.com>
2022-02-09 14:32:53 -0500

    function that prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+
+{
+  n = 0 
+    
+   if(n > 0)
+   {
+     _putchar('+');
+     return(1);
+   }
+  else if (n == 0)
+  {    
+    _putchar('0');
+    return(0);
+    
+  }
+    
+   else  ( n < 0)
+  {
+  _putchar('-');
+    return(-1);
+  }
+}
