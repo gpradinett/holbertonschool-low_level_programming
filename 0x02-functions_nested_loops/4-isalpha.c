c9390fee93a35782cd7a478dd56165548df323df
gpradinett <gpradinett@gmail.com>
2022-02-08 22:07:56 -0800

    adding comment

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,13 @@
+/**
+ * _isalpha - print all alphabet
+ *
+ * @c: char
+ * Return: 1 if alphabetic 0 otherwise
+ */
 int _isalpha(char c)
 {
- if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-   return (1);
-  else
-    return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
