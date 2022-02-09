329b28df87d4dfea90a20e76f36a0d52025dbc1a
gpradinett <94254671+gpradinett@users.noreply.github.com>
2022-02-09 00:06:44 -0500

    cambio en errores

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,16 +15,16 @@ void print_alphabet_x10(void)
   
   i = 0;
   
-	while (i < 10)
-{
-    ch = 'a';
-    while (ch <= 'z')
+    while (i < 10)
     {
-    _putchar(ch);
-      ch++;
-    }
+        ch = 'a';
+        while (ch <= 'z')
+       {
+       _putchar(ch);
+         ch++;
+        }
 	_putchar('\n');
 	i++;	
-}
+    }
 	
 }
