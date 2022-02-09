c4d03bdddebfb163fd1b704e19515d9144032d39
gpradinett <gpradinett@gmail.com>
2022-02-08 19:57:17 -0800

    program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,10 @@
 #include "main.h"
-/*
+/**
+ * main - random number negative and positive random number.
  * main - funtion "_putchar"
+ * Return: Always 0 (two success)
+ *
  * c: char character
- * Return: 0 (Success)
  */
 
 int _putchar(char c);
