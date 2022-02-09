5be0e03d5b447afa1e1fe93f54c5448e9aeaec3c
gpradinett <94254671+gpradinett@users.noreply.github.com>
2022-02-08 23:11:07 -0500

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - random number negative and positive random number.
+ * main - funtion "_putchar"
+ * Return: Always 0 (two success)
+ *
+ * @c: char character
+ */
+
+int _putchar(char c);
+
+int main(void)
+{
+	char ch;
+  ch ='a';
+  
+  while(ch <= 'z')
+  {
+  _putchar(ch);
+    c++;
+  }
+	_putchar('\n');
+	return (0);
+}
