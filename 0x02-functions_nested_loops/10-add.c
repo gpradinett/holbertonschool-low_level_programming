6d180b6d5eae68b8cf72dcbb5d54f6423c74343d
gpradinett <gpradinett@gmail.com>
2022-02-09 18:19:55 -0800

    correcting code error

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * add - add two number integers
+ * @a:	number integer arguments
+ * @b:  number integer arguments
+ * Return: sum of a and b
  */
 
 int add(int a, int b)
 {
- return(a + b);
+	return (a + b);
 }
