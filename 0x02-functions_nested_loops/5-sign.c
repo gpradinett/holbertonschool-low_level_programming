662907546abdcbad5b44a4cb1017660686106114
gpradinett <gpradinett@gmail.com>
2022-02-09 17:41:37 -0800

    change of varible

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,25 @@
 #include "main.h"
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_sign - print number
+ * Return: print 1 if 0, 0 if 0, -1 if less a 0
+ * @n: imput number
  */
 
 int print_sign(int n)
 {
-   if (n > 0)
-   {
-     _putchar('+');
-     return (1);
-   }
-   
-  else if (n == 0)
-  {    
-     _putchar('0');
-     return (0);
-   }
-    
-  else 
-  {
-     _putchar('-');
-     return (-1);
-  }
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar('0');
+	return (0);
+	}
+	else
+	{
+	_putchar('-');
+	return (-1);
+	}
 }
