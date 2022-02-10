01f986ce527a22bf6b1130feea82447c6063872d
gpradinett <gpradinett@gmail.com>
2022-02-09 18:03:46 -0800

    syntax error

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,25 @@
 #include "main.h"
 /**
- * main - check the code
+ * jack_bauer - print the jack-bauer series martaton from 00:00 to 23:59
  *
- * Return: Always 0.
  */
 
 void jack_bauer(void)
 {
-  int h, m;
-  
-  for (h = 0; h < 24; h++)
-  {
-  for(m = 0; m <60; m++)
-  {
-  
-    _putchar((h / 10) + '0');
-    _putchar((h % 10) + '0');
-    _putchar(':');
-    _putchar((m / 10) + '0');
-    _putchar((m % 10) + '0');
-    _putchar('\n');
-  
-  }
-  
-  }
-  
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+	for (m = 0; m < 60; m++)
+	{
+
+	_putchar((h / 10) + '0');
+	_putchar((h % 10) + '0');
+	_putchar(':');
+	_putchar((m / 10) + '0');
+	_putchar((m % 10) + '0');
+	_putchar('\n');
+
+	}
+	}
 }
