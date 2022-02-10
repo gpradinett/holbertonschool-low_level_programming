5e3f0f7a8c4f03aa1efe722b3c1bf541bcedcd59
gpradinett <gpradinett@gmail.com>
2022-02-09 17:50:57 -0800

    change value absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,15 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _abs - calculate the value of the integer
+ * @n: value integer
+ * Return: value absolute
  */
 
 int _abs(int n)
 {
-
-  if (n >= 0)
-  {
-  return (n);
-  }
-  return (-n);
-  
+	if (n >= 0)
+	{
+	return (n);
+	}
+	return (-n);
 }
