4fad332d3b647211079998ae485ee4340a4d5e2d
gpradinett <gpradinett@gmail.com>
2022-02-10 20:58:43 -0800

    syntax error

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * void print_most_numbers - print 0 to the 9 except 2 and 4
+ * print_more_numbers - print 0 to the 9 except 2 and 4
  */
 
-void more_numbers(void)
+void print_more_numbers(void)
 {
 	char i;
 
