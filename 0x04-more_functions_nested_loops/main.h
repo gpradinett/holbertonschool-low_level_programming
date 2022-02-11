4aa2e5af2aa715be0d914ef307a7b9ec4b222a0f
gpradinett <gpradinett@gmail.com>
2022-02-10 21:06:17 -0800

    syntax error

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 
-int _putchar(char c);
+int _putchar(char c)i;
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,5 +13,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
+void print_number(int n);
 #endif /* MAIN_H */
