d58e6f6e2ae1a50c7958ba89e1ace1d205518c88
gpradinett <gpradinett@gmail.com>
2022-02-10 19:35:26 -0800

    add funtion multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - prototype multiplica two intergers
+ * @a: one integer
+ * @b: two integer
+ * Return: value of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * n);
+}
