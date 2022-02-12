36ca77084b66759eeb23c25a91839eedcb80e310
gpradinett <gpradinett@gmail.com>
2022-02-11 18:00:23 -0800

    syntax error

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - print caracter \ n times
+ * @n: number caracter to draw
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n < 100)
+		{
+			if ((n % 3 == 0) && !(n % 5 == 0))
+			{
+			printf ("Fizz ");
+			}
+			else if ((n % 5 == 0) &&  !(n % 3 == 0 ))
+			{
+			printf ("Buzz ");
+			}
+			else if ((n % 3 == 0) && (n % 5 == 0))
+			{
+			printf ("Fizz Buzz");
+			}
+			else 
+			{
+			printf ("%d", n);
+			}
+		
+			if (n == 100)
+			{
+			printf ("Buzz \n");
+			}
+		}	
+		
+	}
+	return (0);
+}
