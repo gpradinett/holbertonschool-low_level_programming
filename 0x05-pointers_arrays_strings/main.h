9c63c90d0da4dfe0f3d3078c2835b39554b7d696
gpradinett <gpradinett@gmail.com>
2022-02-14 16:37:34 -0800

    add _putchar

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _putchar - puts character to standar output
+ * @c: character to put to standar output
+ */
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
