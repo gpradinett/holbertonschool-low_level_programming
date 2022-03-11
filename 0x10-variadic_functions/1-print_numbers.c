5d12773352733e8d1acc76b39492e9bad558999e
gpradientt <gpradinett@gmail.com>
2022-03-10 23:11:26 -0500

    change parametro

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+nclude "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
+ void print_numbers(const char *separator, const unsigned int n, ...)
+{
+		unsigned int i;
+		int valores;
+
+		va_list parametros;
+
+		va_start(parametros, n);
+		for (i = 0; i < n; i++)
+		{
+		valores = va_arg(parametros, int);
+
+		if (separator == NULL || i == 0)
+			printf("%d", valores);
+		else
+			printf("%s%d", separator, valores);
+	}
+	va_end(parametros);
+	printf("\n");
+}
