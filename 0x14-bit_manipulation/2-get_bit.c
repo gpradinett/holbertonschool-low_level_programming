800887712f4b10da8def550f1a6e649671c209ae
gpradientt <gpradinett@gmail.com>
2022-03-24 23:31:52 -0500

    add coment

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,12 +8,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int bits;
+	int bit_val;
 
-	bits = (n >> index) & 1;
-
-	if ((bits == 1) || (bits == 0))
-		return (bits);
-	else
+	if (index > 63)
 		return (-1);
+
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
 }
